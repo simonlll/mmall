3205c5c4d7bffd3db2a140313fdbc9a3cb79e21c
binlin <33875983@qq.com>
2018-09-24 09:48:30 +0800

    redis lock

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -28,6 +28,7 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
+    @Scheduled(cron = "0 */1 * * * ?") //每一分钟（每个一分钟的整数倍）
     public void closeOrderTaskV2() {
         log.info("关闭订单定时任务启动");
         long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "5000"));
@@ -44,7 +45,7 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
-    @Scheduled(cron = "0 */1 * * * ?") //每一分钟（每个一分钟的整数倍）
+
     private void closeOrder(String lockName) {
         RedisShardedPoolUtil.expire(lockName, 50); //有效期50秒，防止死锁
         log.info("获取{}, ThreadName:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
